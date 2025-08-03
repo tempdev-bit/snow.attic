5d884f4a1a6610f43fbf78d8b5125549f4f7722b
tempdev-bit <copperwaregamedev@gmail.com>
2025-08-03 13:20:20 +0530

    import stuff

--- server.py
+++ server.py
@@ -1,1 +1,19 @@
+try:
+    import sys
+    from flask import Flask, render_template, request, redirect, url_for, send_from_directory, abort
+    from flask_httpauth import HTTPBasicAuth
+    from dotenv import load_dotenv
+    import os
+except ImportError as e:
+    print("Test 1 failed: Import error: {e}")
+    import traceback
+    traceback.print_exc()
+else:
+    print(f"Passed Test 1!")
 
+#Try to import pyngrok for public access; PURELY OPTIONAL
+try:
+    from pyngrok import ngrok
+except ImportError:
+    ngrok = None
+    print(f"Couldn't load ngrok; no web access")
