8de0200fda8752772678b2c99b3447d4db21d04a
tempdev-bit <copperwaregamedev@gmail.com>
2025-08-03 14:04:18 +0530

    Basic authentication
    index.html - TODO

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ try:
     import sys
     from flask import Flask, render_template, request, redirect, url_for, send_from_directory, abort
     from flask_httpauth import HTTPBasicAuth
+    from werkzeug.security import generate_password_hash, check_password_hash
+    from werkzeug.utils import secure_filename
     from dotenv import load_dotenv
     import os
 except ImportError as e:
@@ -32,3 +34,30 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 #Allowed file extensions
 ALLOWED_EXTENSIONS = {'txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif', 'zip', 'mp3', 'mp4', 'gba'}
+
+#Basic Auth setup
+auth = HTTPBasicAuth()
+user = {
+    os.getenv("SNOW_USERNAME"):
+    generate_password_hash(os.getenv("SNOW_PASSWORD"))
+    }
+
+#Check user creds
+@auth.verify_password
+def verify_password(username, password):
+    print(f"Authenticating user...")
+    if username in users and check_password_hash(users[username], password):
+        return username
+        print(f"User authenticated!")
+
+#Validate safe file types
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit( '.', 1)[1].lower in ALLOWED_EXTENSIONS
+
+#Homepage
+@app.route('/')
+@auth.login_required
+def index():
+    files = sorted(os.listdir(app.config['UPLOAD_FOLDER']))
+    return render_template('index.html', files=files) #TODO - index.html
+
